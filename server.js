daa15e8b6de0410103e7795a5f1ad508d18e3969
christinejoyilonen65-droid <christinejoyilonen65@gmail.com>
2026-07-22 19:36:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import { GoogleGenAI } from "@google/genai";
 import dotenv from 'dotenv';
 
 dotenv.config();
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 console.log("GEMINI_API_KEY:", process.env.GEMINI_API_KEY);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
